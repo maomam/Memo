184f37ddba3b8190b22431440faf690fac5936c4
Daryna <darynka@mail.ua>
2013-04-09 16:32:39 +0200

    Tests
    tests implemented. still branch coverage needed

--- test/testModel/TestField.scala
+++ test/testModel/TestField.scala
@@ -4,6 +4,7 @@ import org.junit.runner.RunWith
 import model._
 import util._
 import org.specs2.mutable._
+import scala.collection.immutable.ListMap
 
 //@RunWith(classOf[JUnitRunner])
 class TestField extends SpecificationWithJUnit {
@@ -11,11 +12,21 @@ class TestField extends SpecificationWithJUnit {
     val field = new Feld(2, Theme.fruits)
     "dimension" in {
       field.dimension must be_==(2)
+      field(0,0).pictureNr must not be_==(0)
+     field(1,1).pictureNr must not be_==(0)
+     field.anzahlZellen must be_==(3)
+       
     }
 
     "newTheme" in {
       field.setTheme(Theme.people)
       field.currentTheme must be_==(Theme.people)
+    }
+    
+     "reset Field" in {
+      field.reset
+      field(1,1).guessed must be_==(false)
+       field(0,0).guessed must be_==(false)
     }
 
     "gameOver" in {
@@ -61,36 +72,43 @@ class TestField extends SpecificationWithJUnit {
 
   "guessed field" should {
     val field = new Feld(2, Theme.fruits)
-    var one =field(0,0).pictureNr
-    var two =field(0,0).pictureNr
-    field.tryOpen(0, 0)
-    field.tryOpen(1, 1)
-    field.tryOpen(1, 1)
-
-    field.tryOpen(1, 0)
-    field.tryOpen(0, 1)
-
-    field.tryOpen(0, 0)
-    field.tryOpen(1, 0)
-
-    field.tryOpen(0, 1)
-    field.tryOpen(1, 1)
-
-    field.tryOpen(1, 0)
-    field.tryOpen(1, 1)
-
-    field.tryOpen(0, 0)
-    field.tryOpen(0, 1)
-
+    var coordinates = ListMap(Map((0, 0) -> field(0, 0).pictureNr, (0, 1) -> field(0, 1).pictureNr, (1, 1) -> field(1, 1).pictureNr, (1, 0) -> field(1, 0).pictureNr).toList.sortBy { _._2 }: _*)
+    var keys = coordinates.keys.toSeq
+    var coordsone: Coordinates = keys(0)
+    var coordstwo: Coordinates = keys(1)
+    var coordsthree: Coordinates = keys(2)
+    var coordsfour: Coordinates = keys(3)
+    field.tryOpen(coordsone._1, coordsone._2)
+    field.tryOpen(coordsthree._1, coordsthree._2)
+    field.tryOpen(coordstwo._1, coordstwo._2)
+    field.tryOpen(coordstwo._1, coordstwo._2)
+     //two times the same cell chosen
+    //three not guessed. different pictureNr
+    field.tryOpen(coordsfour._1, coordsfour._2)
+      //four not guessed
+    field.tryOpen(coordsone._1, coordsone._2)
+    field.tryOpen(coordstwo._1, coordstwo._2)
+     //one and two are now guessed
+    field.tryOpen(coordsone._1, coordsone._2)
+    field.tryOpen(coordstwo._1, coordstwo._2)
+     //guessed cell clicked
+    field.tryOpen(coordsthree._1, coordsthree._2)
+    field.tryOpen(coordsfour._1, coordsfour._2)
+    //all guessed game over
     "field hit" in {
       field(0, 0).guessed must be_==(true)
       field(1, 0).guessed must be_==(true)
       field(0, 1).guessed must be_==(true)
       field(1, 1).guessed must be_==(true)
       field.gameOver must be_==(true)
+      field.gameIsOver must be_==(true)
 
     }
   }
-  
-  
+   "4 dimension Field" should {
+    val field = new Feld(4, Theme.countries)
+    "dimension" in {
+      field.dimension must be_==(4)
+    }
+   }
 }
